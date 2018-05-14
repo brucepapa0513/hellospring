fe695282c6c64d5f6554a8b904fb0491ba8e57f9
bruceguo0513 <66039498@qq.com>
2018-05-14 23:55:12 +0800

    2018-05-14 commit test

--- src/test/java/com/bruce/hellospring/demo2/CDPlayerTest.java
+++ src/test/java/com/bruce/hellospring/demo2/CDPlayerTest.java
@@ -3,17 +3,17 @@ package com.bruce.hellospring.demo2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
 
-    @Resource
+    @Autowired
     private List<CompactDisc> compactDiscList;
 
     @Test
